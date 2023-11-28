013c832cee38d5351715b5bd03e5399b45447196
GI000000 <giogio0570@gmail.com>
2023-11-28 16:35:52 +0400

    optimized image sizes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,10 +7,10 @@ function Footer() {
     <div className={styles.wrap}>
       <div>All Rights Reserved.</div>
       <div className={styles.iconWrap}>
-        <Image src="/1.png" width={35} height={35} alt="facebook icon" className={styles.icon} />
-        <Image src="/2.png" width={35} height={35} alt="instagram icon" className={styles.icon} />
-        <Image src="/3.png" width={35} height={35} alt="tweeter icon" className={styles.icon} />
-        <Image src="/4.png" width={35} height={35} alt="youtube icon" className={styles.icon} /> 
+        <Image src="/1.png" width={25} height={25} alt="facebook icon" className={styles.icon} />
+        <Image src="/2.png" width={25} height={25} alt="instagram icon" className={styles.icon} />
+        <Image src="/3.png" width={25} height={25} alt="tweeter icon" className={styles.icon} />
+        <Image src="/4.png" width={25} height={25} alt="youtube icon" className={styles.icon} /> 
       </div>
     </div>
   )
