f2c1f4a5bf756dce88763cd30f916ac3587de892
GI000000 <giogio0570@gmail.com>
2023-11-29 15:56:41 +0400

    changed navigation elements

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,46 @@
 import React from 'react'
 import styles from "./navbar.module.css"
+import Link from 'next/link'
 
 function Navbar() {
   const links = [
     {
       id: 1,
+      url: '/',
       title: "Home"
     },
     {
       id: 2,
-      title: "About"
+      url: '/portfolio',
+      title: "Portfolio"
     },
     {
       id: 3,
-      title: "Contact Us"
+      url: '/blog',
+      title: "Blog"
+    },
+    {
+      id: 4,
+      url: '/about',
+      title: "About"
+    },
+    {
+      id: 5,
+      url: '/contact',
+      title: "Contact"
+    },
+    {
+      id: 6,
+      url: '/dashboard',
+      title: "Dashboard"
     }
   ];
-
   return (
     <div className={styles.links}>
-      {links.map(link => (<h1>{link.title}</h1>))}
+      <div>Main</div>
+      <div className={styles.links}>
+        {links.map(link => (<Link href={link.url}>{link.title}</Link>))}
+      </div>
     </div>
   )
 }
