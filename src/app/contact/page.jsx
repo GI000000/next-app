8d74cf3f2ba315cf090df19816d910b9bbf469b1
GI000000 <giogio0570@gmail.com>
2023-12-06 17:16:40 +0400

    added contact page markup

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import styles from "./page.module.css"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <main>
+      <h1>Let's Keep in Touch</h1>
+      <div></div>
+      <form action="">
+
+        <input type="text" />
+        <input type="text" />
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </form>
+    </main>
   )
 }
 
