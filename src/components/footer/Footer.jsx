43a1b91b45cb7f641424664f73c5840dfc4cde64
GI000000 <giogio0570@gmail.com>
2023-12-06 22:45:57 +0400

    optimized footer design

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,10 +8,10 @@ function Footer() {
     <div className={styles.wrap}>
       <div>©All Rights Reserved.</div>
       <div className={styles.iconWrap}>
-        <Link href="https://www.facebook.com/" target='blank'><Image src="/1.png" width={25} height={25} alt="facebook icon" className={styles.icon} /></Link>
-        <Link href="https://www.instagram.com/" target='blank'><Image src="/2.png" width={25} height={25} alt="instagram icon" className={styles.icon} /></Link>
-        <Link href="https://twitter.com/" target='blank'><Image src="/3.png" width={25} height={25} alt="tweeter icon" className={styles.icon} /></Link>
-        <Link href="https://www.youtube.com/" target='blank'><Image src="/4.png" width={25} height={25} alt="youtube icon" className={styles.icon} /></Link> 
+        <Link href="https://www.facebook.com/" target='blank' className={styles.icon}><Image src="/1.png" width={25} height={25} alt="facebook icon" /></Link>
+        <Link href="https://www.instagram.com/" target='blank' className={styles.icon}><Image src="/2.png" width={25} height={25} alt="instagram icon" /></Link>
+        <Link href="https://twitter.com/" target='blank' className={styles.icon}><Image src="/3.png" width={25} height={25} alt="tweeter icon" /></Link>
+        <Link href="https://www.youtube.com/" target='blank' className={styles.icon}><Image src="/4.png" width={25} height={25} alt="youtube icon" /></Link> 
       </div>
     </div>
   )
