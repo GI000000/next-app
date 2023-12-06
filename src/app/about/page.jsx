e894e0334c84ba71f90b89fc03204cd6c8a051c1
GI000000 <giogio0570@gmail.com>
2023-12-06 17:21:21 +0400

    added Button component to pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,4 @@
+import Button from '@/components/button/Button'
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
@@ -12,6 +13,44 @@ function About() {
           <h2>Handcrafting award winning digital experiences</h2>
         </div>
       </div>
+      <section className={styles.textContainer}>
+        <div>
+          <h1>Who Are We</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae. A
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Ducimus quae dolor, optio voluptatibus magnam iure esse tempora
+            beatae, a suscipit eos. Animi quibusdam cum omnis officiis
+            <br />
+            <br />
+            voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+            esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+            officiis voluptatum quo ea eveniet?
+          </p>
+        </div>
+        <div>
+          <h1>What We Do?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            quae dolor, optio voluptatibus magnam iure esse tempora beatae, a
+            suscipit eos. Animi quibusdam cum omnis officiis voluptatum quo ea
+            eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. -
+            Creative Illustrations
+            <br /> <br />
+            - Dynamic Websites
+            <br />
+            <br />
+            - Fast and Handy
+            <br />
+            <br />
+            - Mobile Apps
+          </p>
+          <Button url={'/contact'} title={"Contact Us"} />
+        </div>
+      </section>
     </main>
   )
 }
